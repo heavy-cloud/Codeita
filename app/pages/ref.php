487467459cc1e437b164f23af5b677d5a0581f5a
Christian Beier <office@beier-christian.eu>
2011-09-30 13:44:04 +0200

    remove stackoverflow from reference, because of a framebreaker

--- app/pages/ref.php
+++ app/pages/ref.php
@@ -3,7 +3,6 @@
 		<li><a target="ref-iframe" href="http://php.net">PHP.net</a></li>
 		<li><a target="ref-iframe" href="http://dev.w3.org/html5/spec/Overview.html">HTML5 Spec</a></li>
 		<li><a target="ref-iframe" href="http://w3schools.com/">W3 Schools</a></li>
-		<li><a target="ref-iframe" href="http://stackoverflow.com">Stack Overflow</a></li>
 	</ul>
 </div>
 <div id="ref-frame-wrap">
